08a03491b8760017c37488d167ad2720028a2a10
Lucas Cruz <lucascs20182@gmail.com>
2021-07-13 15:26:19 -0300

    adicione BottomTabNavigator à tela inicial

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import Produtos from '../Produtos';
+import Carrinho from '../Carrinho';
+
+const { Navigator, Screen } = createBottomTabNavigator();
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>tela inicial</Text>
-    </View>
+    <Navigator tabBarOptions={{activeTintColor: '#e91e63'}}>    
+      <Screen
+        name="Produtos" component={Produtos}
+        options={{ tabBarIcon: () => (
+            <MaterialCommunityIcons name="home" color={"black"} size={30} />
+        )}}
+      />
+
+      <Screen
+        name="Carrinho" component={Carrinho}
+        options={{ tabBarIcon: () => (
+            <MaterialCommunityIcons name="home" color={"black"} size={30} />
+        )}}
+      />
+    </Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
