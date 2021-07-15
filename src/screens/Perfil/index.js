d515c7a61f349118884cf41484d080d3c165e95f
IT-Leandro-Camargo <it-leandro-camargo@gmail.com>
2021-07-15 18:09:13 -0300

    implemente a tela de perfil

--- src/screens/Perfil/index.js
+++ src/screens/Perfil/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { deleteKey } from '../../storage';
 
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { View, Text, Button, Image, TouchableOpacity, } from 'react-native';
+import styles from './style';
 
 export default function Perfil({ navigation }) {
 
@@ -13,17 +14,41 @@ export default function Perfil({ navigation }) {
     }
 
     return (
-        <View style={styles.container}>
-            <Text>perfil</Text>
-            <Button title="Logout" onPress={() => handleLogout()} />
+        <View style={styles.containerBackground}>
+            <View style={styles.backgroundBlue}>
+                <Image style={styles.logo} source={require('../../resources/logo.png')} />            
+                <View style={styles.backgroundWhite}>
+                    <View style={styles.container}>                        
+                        <Text style={styles.text}>Perfil</Text>
+                        <Image style={styles.photo} source={require('../../resources/foto-mari.jpeg')} />
+                        <Text style={styles.userName}>Mari Sá</Text>
+                        <Text style={styles.userInfo}>E-mail: mari.sa@gmail.com</Text>
+                        <Text style={styles.userInfo}>Username: Mari_Sa</Text>
+                        <Text style={styles.userInfo}>Senha: ••••</Text>
+                        <Text style={styles.userInfo}>CPF: 012.345.678-90</Text>
+                        <Text style={styles.userInfo}>Endereço: Rua dos Astros, 50, Casa 2,</Text>
+                        <Text style={styles.userInfo}>Teresópolis - Rio de Janeiro</Text>
+                        <Text style={styles.userInfo}>CEP: 25953-203</Text>
+                        
+                        <TouchableOpacity onPress={handleLogout}
+                            style={styles.containerBotaoEntrar}>
+                            <Text style={styles.botaoEntrar}>SAIR</Text>
+                        </TouchableOpacity>
+                        
+                        {/* <View style={styles.button}>
+                            <Button title="Sair" onPress={() => handleLogout()} />
+                        </View> */}
+                    </View>
+                </View>
+            </View> 
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//     },
+// });
